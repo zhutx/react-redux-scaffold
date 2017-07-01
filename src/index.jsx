a0ca8df53f399211d3745f94a51b2e127da68a08
zhutx <16530202@qq.com>
2017-07-01 20:30:58 +0800

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DatePicker } from 'antd';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'normalize.css';
+
+moment.locale('zh-cn');
+
+function Index() {
+    return (
+      <div className="container">
+        <h1>Antd DatePicker!</h1>
+        <DatePicker defaultValue={moment('2015-01-01', 'YYYY-MM-DD')} />
+      </div>
+    );
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+export default Index;
